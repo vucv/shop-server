747a240675d72b53f58979eed5b38bd040349055
Cap Van Vu <capvanvu@gmail.com>
2016-03-28 14:39:03 +0700

    .

--- app.js
+++ app.js
@@ -121,8 +121,9 @@ connection.query("CREATE DATABASE IF NOT EXISTS " + DB_CONFIG.name, function (er
     console.log('Error: ' + err);
 });
 
-var query = 'DROP TABLE sync_info';
+var query = 'DROP TABLE IF EXISTS sync_info';
 connection.query(query, function (err) {
+    console.log('Error: ' + err);
     console.log('Error: ' + query);
 });
 
